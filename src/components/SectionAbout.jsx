a35d1784ff91ef33fe70519083141785784a188f
Mouss <moussapro.dp@gmail.com>
2024-11-07 16:02:35 +0100

    Page à propos = done ✅

--- src/components/SectionAbout.jsx
+++ src/components/SectionAbout.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react"
 import '../styles/SectionAbout.css'
-function Section1(){
+import apropos from '../datas/apropos.json'
+import imgUp from '../assets/chevron-up.svg'
+import imgDown from '../assets/chevron-down.svg'
+
+
+function SectionABout(){
+
+    const [openItems, setOpenItems] = useState({})
+
+    const toggleItem = (index) => {
+        setOpenItems((prev) => ({...prev, [index]: !prev[index]}))
+    }
 
     return (
-        <div className='section_about'>
-            <div className='opacity_about'>
+        <div>
+            <div className='section_about'>
+                <div className='opacity_about'>
+                </div>
             </div>
+            <ul>
+                {apropos.map(({title, content}, index)=> (
+                    <li key={index} className={`section_about_li ${openItems[index] ? 'open' : ''}`}>
+                        <div>
+                            <h2>{title}</h2>
+                            <img src={openItems[index] ? imgDown : imgUp} alt="chevron" onClick={() => toggleItem(index)} />
+                        </div>
+                        {openItems[index] && <p className="section_about_content">{content}</p>}
+
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
 
-export default Section1
+export default SectionABout
